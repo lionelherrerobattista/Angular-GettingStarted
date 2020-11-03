2dd1a01419997f6be23c2abcca78609d734494e6
Lionel Herrero Battista <lionherrero@gmail.com>
2020-11-03 17:17:44 -0300

    Modulo 13

---
+++ APM-Start/src/app/products/products.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { IProduct } from './product';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ProductService {
+
+    private productUrl = 'api/products/products.json';
+
+    constructor (private http: HttpClient) { }
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data => console.log('All: ' + JSON.stringify(data))),
+            catchError(this.handleError),
+        )
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        let errorMessage = '';
+        if(err.error instanceof ErrorEvent) {
+            //A client-side or network error occurred.
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            //The backend returned an unsuccessful response code.
+            //The resonse body may contain clues as to what went wrong.
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+    }
+}
